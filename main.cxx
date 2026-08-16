b4b5d6b5e2f3fabc667b2bcad2efc9a0cc24e5de
Juan123839 <diegoare20032@gmail.com>
2026-08-15 22:16:46 -0500

    Creacion de recibir comando atacar y costo_conquista

--- main.cxx
+++ main.cxx
@@ -39,6 +39,34 @@ int main() {
             }
             break;
         }
+        if (tokens[0] == "estado_juego") {
+            if (tokens.size() > 1) {
+                cout << "Error: El comando 'estado_juego' no debe tener argumentos" << endl;
+                continue;
+            }
+            cout << "Comando estado de juego: reconocido" << endl;
+            continue;
+        }
+        for (int i = 0; i < tokens.size(); i++) {
+            cout << tokens[i] << endl;
+        }
+
+        if (tokens[0] == "atacar") {
+            if (tokens.size() != 2) {
+                cout << "Error de uso. Uso correcto: atacar nombre_jugador" << endl;
+                continue;
+            }
+            cout << "Comando atacar: reconocido" << endl;
+            continue;
+        }
+        if (tokens[0] == "costo_conquista") {
+            if (tokens.size() != 3) {
+                cout << "Error de uso. Uso correcto: costo_conquista nombre_jugador territorio" << endl;
+                continue;
+            }
+            cout << "Comando costo_conquista: reconocido" << endl;
+            continue;
+        }
         for (int i = 0; i < tokens.size(); i++) {
             cout << tokens[i] << endl;
         }
