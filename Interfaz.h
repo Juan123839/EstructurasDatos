ae7e8ecec8d6c6a9ec56849a1c0bbfb91beec968
Juan123839 <diegoare20032@gmail.com>
2026-08-15 23:19:41 -0500

    se organizo la funcion separar tokens en un archivo .h como header y un archivo .cxx que es la logica

---
+++ Interfaz.h
@@ -0,0 +1,11 @@
+#ifndef INTERFAZ_H
+#define INTERFAZ_H
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> separarTokens(string linea);
+
+#endif
--- main.cxx
+++ main.cxx
@@ -1,28 +1,10 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include "Interfaz.h"
 
 using namespace std;
 
-vector<string> separarTokens(string linea) {
-    vector<string> tokens;
-        string palabra = "";
-
-        for (int i = 0; i < linea.size(); i++) {
-            if (linea[i] == ' ') {
-                if (!palabra.empty()) {
-                    tokens.push_back(palabra);
-                    palabra = "";
-                }
-            } else {
-                palabra += linea[i];
-            }
-        }
-        if (!palabra.empty()) {
-            tokens.push_back(palabra);
-        }
-    return tokens;
-}
 int main() {
 
     string linea;
