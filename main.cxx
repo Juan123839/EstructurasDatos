087d73cfb97705295010d9cd6481ee5d09755ce5
Juan123839 <diegoare20032@gmail.com>
2026-08-15 20:46:52 -0500

    sacar cada palabras por tokens con funcion tokens de los vectores

--- main.cxx
+++ main.cxx
@@ -1,17 +1,30 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
+
 
 int main() {
 
     string linea;
 
     while (true) {
-
         cout << "$ ";
-
         getline(cin, linea);
+        vector<string> tokens;
+        string palabra = "";
+
+        for (int i = 0; i < linea.size(); i++) {
+            if (linea[i] == ' ') {
+                if (!palabra.empty()) {
+                    tokens.push_back(palabra);
+                    palabra = "";
+                }
+            } else {
+                palabra += linea[i];
+            }
+        }
 
         cout << "Escribiste: " << linea << endl;
     }
